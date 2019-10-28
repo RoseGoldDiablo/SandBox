84fae2afce166d5d40627713f8fbd6937665f610
RoseGoldDiablo <53830216+RoseGoldDiablo@users.noreply.github.com>
2019-10-28 16:22:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,78 +4,91 @@ import sys
 
 
 class Button(QPushButton):
-    width = 75
-    height = 35
     button = None
-
+    command = None
+    
     def __init__(self, window, x, y, text):
         super().__init__()
         self.window = window
         self.x = x
         self.y = y
         self.text = text
-
+    
     def makeButton(self):
         self.button = QPushButton(self.text, self.window)
         self.button.move(self.x, self.y)
+    
+    def onClick(self, command):
+        self.button.clicked.connect(command)
 
 
 class Label(QLabel):
     label = None
-
+    font = 'Source Code Pro'
+    fontSize = '20pt'
+    
     def __init__(self, window, text, x, y):
         super().__init__()
         self.window = window
         self.text = text
         self.x = x
         self.y = y
-
+    
     def makeLabel(self):
         self.label = QLabel(self.text, self.window)
+        self.label.setStyleSheet("font: " + self.fontSize + " " + self.font)
         self.label.move(self.x, self.y)
 
 
 class TextBox(QLineEdit):
     textbox = None
-
+    
     def __init__(self, window, x, y, default):
         super().__init__()
         self.window = window
         self.x = x
         self.y = y
         self.default = default
-
+    
     def makeTextBox(self):
         self.textbox = QLineEdit(self.default, self.window)
         self.textbox.move(self.x, self.y)
 
 
-
-
-class ToolBox(QDialog):
+class ui(QDialog):
     def __init__(self):
         super().__init__()
         self.title = 'ToolBox'
-        self.left = 10
-        self.top = 10
+        self.left = 200
+        self.top = 100
         self.width = 650
         self.height = 500
-        self.mainMenu()
-
-    def mainMenu(self):
+        self.Welcome()
+    
+    def Welcome(self):
+        def close():
+            ContinueButton.clear()
+            titleLabel.clear()
+            
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        softwareButton = Button(self, 200, 100, 'Software')
-        softwareButton.makeButton()
-        titleLabel = Label(self, 'Hello', 150, 75)
+        ContinueButton = Button(self, 280, 380, 'Continue')
+        ContinueButton.makeButton()
+        ContinueButton.onClick(close)
+        ContinueButton.onClick(self.MainMenu)
+        titleLabel = Label(self, 'ToolBox', 270, 75)
         titleLabel.makeLabel()
-        testEdit = TextBox(self, 200, 300, 'Enter Here')
-        testEdit.makeTextBox()
+        
         self.show()
-
-
+    
+    def MainMenu(self):
+        self.title = 'Main Menu'
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    exe = ToolBox()
+    exe = ui()
     sys.exit(app.exec_())
+
