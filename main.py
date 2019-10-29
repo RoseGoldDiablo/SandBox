7bfd58ac95ee9ce534b76fc689765413dbfae537
RoseGoldDiablo <53830216+RoseGoldDiablo@users.noreply.github.com>
2019-10-28 23:04:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,59 +3,24 @@ from PyQt5.QtCore import *
 import sys
 
 
-class Button(QPushButton):
-    button = None
-    command = None
-    
-    def __init__(self, window, x, y, text):
-        super().__init__()
-        self.window = window
-        self.x = x
-        self.y = y
-        self.text = text
-    
-    def makeButton(self):
-        self.button = QPushButton(self.text, self.window)
-        self.button.move(self.x, self.y)
-    
-    def onClick(self, command):
-        self.button.clicked.connect(command)
+# Function for deleting all items in  layout
+def clearLayout(layout):
+    while layout.count():
+        child = layout.takeAt(0)
+        if child.widget() is not None:
+            child.widget().deleteLater()
+        elif child.layout() is not None:
+            clearLayout(child.layout())
 
 
-class Label(QLabel):
-    label = None
-    font = 'Source Code Pro'
-    fontSize = '20pt'
-    
-    def __init__(self, window, text, x, y):
-        super().__init__()
-        self.window = window
-        self.text = text
-        self.x = x
-        self.y = y
-    
-    def makeLabel(self):
-        self.label = QLabel(self.text, self.window)
-        self.label.setStyleSheet("font: " + self.fontSize + " " + self.font)
-        self.label.move(self.x, self.y)
+# ----------------------------------------------------------------------------------------------------------------------
 
 
-class TextBox(QLineEdit):
-    textbox = None
-    
-    def __init__(self, window, x, y, default):
-        super().__init__()
-        self.window = window
-        self.x = x
-        self.y = y
-        self.default = default
-    
-    def makeTextBox(self):
-        self.textbox = QLineEdit(self.default, self.window)
-        self.textbox.move(self.x, self.y)
-
-
-class ui(QDialog):
+# Class: UI (user interface)
+# Description: Used to house the basics for setting up User Interface
+class UI(QWidget):
+    vlayout = None
+    hlayout = None
     def __init__(self):
         super().__init__()
         self.title = 'ToolBox'
@@ -64,31 +29,69 @@ class ui(QDialog):
         self.width = 650
         self.height = 500
         self.Welcome()
-    
-    def Welcome(self):
-        def close():
-            ContinueButton.clear()
-            titleLabel.clear()
-            
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        ContinueButton = Button(self, 280, 380, 'Continue')
-        ContinueButton.makeButton()
-        ContinueButton.onClick(close)
-        ContinueButton.onClick(self.MainMenu)
-        titleLabel = Label(self, 'ToolBox', 270, 75)
-        titleLabel.makeLabel()
-        
-        self.show()
-    
-    def MainMenu(self):
-        self.title = 'Main Menu'
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        
 
+    # Function for creating the Welcome window. Sets the window size, location on the screen, title for the window
+    # and all the buttons and labels on the window
+
+    def Welcome(self):
+        # Function for closing the Welcome window
+        def NextWindow():
+            clearLayout(hlayout)
+            clearLayout(vlayout)
+            # Calls the next menu
+            self.MainMenu()
+
+        # --------------------------------------------------------------------------------------------------------------
+
+        # Defining the layout
+        global vlayout
+        global hlayout
+        vlayout = QVBoxLayout()
+        hlayout = QHBoxLayout()
+        self.setLayout(vlayout)
+        # --------------------------------------------------------------------------------------------------------------
+        # Widgets
+        WelcomeTitle = QLabel('Welcome')
+        ContinueButton = QPushButton('Continue')
+        # --------------------------------------------------------------------------------------------------------------
+        # Setting the Layout (Vertical)
+        vlayout.addStretch()
+        vlayout.addWidget(WelcomeTitle)
+        vlayout.addStretch()
+        # --------------------------------------------------------------------------------------------------------------
+        # Setting the Layout (Horizontal)
+        hlayout.addStretch()
+        hlayout.addWidget(ContinueButton)
+        hlayout.addStretch()
+        vlayout.addLayout(hlayout)
+        vlayout.addStretch()
+        # --------------------------------------------------------------------------------------------------------------
+        # Aligns the text in label to the center
+        WelcomeTitle.setAlignment(Qt.AlignHCenter)
+        # --------------------------------------------------------------------------------------------------------------
+        ContinueButton.clicked.connect(NextWindow)
+        # Sets and shows the window
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+        # --------------------------------------------------------------------------------------------------------------
+
+    # Function for creating the Main Menu window.
+    def MainMenu(self):
+        global vlayout
+        global hlayout
+        self.title = 'Main Menu'
+        MainMenuTitle = QLabel('Main Menu')
+        vlayout.addStretch()
+        vlayout.addWidget(MainMenuTitle)
+        vlayout.addStretch()
+        MainMenuTitle.setAlignment(Qt.AlignCenter)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowTitle(self.title)
+
+
+# Function that starts the application
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    exe = ui()
+    exe = UI()
     sys.exit(app.exec_())
-
