390667dc9ff84116550253433975b259b653bb89
RoseGoldDiablo <53830216+RoseGoldDiablo@users.noreply.github.com>
2019-10-31 08:47:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class UI(QWidget):
     # Setting global layout variables
     Vertical = None
     Horizontal = None
-
+    
     # Initialize
     def __init__(self):
         super().__init__()
@@ -36,21 +36,26 @@ class UI(QWidget):
         self.width = 650
         self.height = 500
         self.Welcome()
-
+        
         # --------------------------------------------------------------------------------------------------------------
-
+    
     # Function for creating the Welcome window. Sets the window size, location on the screen, title for the window
     # and all the buttons and labels on the window
     def Welcome(self):
-        # Function for closing the Welcome window
+        # Function for authenticating the password and closing the Welcome window if the correct password was entered
         def NextWindow():
-            clearLayout(Horizontal)
-            clearLayout(Vertical)
-            # Calls the next menu
-            self.MainMenu()
-
+            if TEPassword.text() == 'Test':
+                
+                clearLayout(Horizontal)
+                clearLayout(Vertical)
+            
+                # Calls the next menu
+                self.MainMenu()
+        
+            else:
+                TEPassword.setText('Wrong')
         # --------------------------------------------------------------------------------------------------------------
-
+        
         # Defining the layout
         global Vertical
         global Horizontal
@@ -72,8 +77,7 @@ class UI(QWidget):
         Horizontal.addWidget(TEPassword)
         Horizontal.addStretch()
         Vertical.addLayout(Horizontal)
-
-
+        
         # --------------------------------------------------------------------------------------------------------------
         # Setting the Layout (Horizontal)
         Horizontal2.addStretch()
@@ -91,40 +95,65 @@ class UI(QWidget):
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
         # --------------------------------------------------------------------------------------------------------------
-
+    
     # Function for creating the Main Menu window.
     def MainMenu(self):
         # Layout globals
         global Vertical
         global Horizontal
+        global Grid
+        super().__init__()
+        # Constructing Window title, size, and position
+        self.title = 'ToolBox'
+        self.left = 200
+        self.top = 100
+        self.width = 650
+        self.height = 500
+        self.Welcome()
 
+        # ----------------------------------------------------
+        
         # Clears layouts and calls Software Loop-er menu
-        def SLNext():
+        def CleanUp():
             clearLayout(Vertical)
             clearLayout(Horizontal)
+            clearLayout(Grid)
+            
+        def SLNext():
+            CleanUp()
             self.SLMenu()
-
+            
+        def P12Next():
+            CleanUp()
+            self.P12Menu()
+        
         # --------------------------------------------------------------------------------------------------------------
         # Sets some changes for the the menu like adding the buttons and changing the window title
         self.title = 'ToolBox'
         SoftwareLoopButton = QPushButton('Software Looper')
+        DeleteP12 = QPushButton('P12 Removal')
+        Option2 = QPushButton('Option2')
+        Option3 = QPushButton('Option3')
+        
         # --------------------------------------------------------------------------------------------------------------
         # Sets the layout for the window
+        Grid = QGridLayout()
         Vertical.addStretch()
-        Vertical.addStretch()
-        Horizontal.addStretch()
-        Horizontal.addWidget(SoftwareLoopButton)
-        Horizontal.addStretch()
-        Vertical.addLayout(Horizontal)
+        Grid.addWidget(SoftwareLoopButton, 0, 0)
+        Grid.addWidget(DeleteP12, 0, 1)
+        Grid.addWidget(Option2, 1, 0)
+        Grid.addWidget(Option3, 1, 1)
+        Vertical.addLayout(Grid)
         Vertical.addStretch()
         SoftwareLoopButton.clicked.connect(SLNext)
+        DeleteP12.clicked.connect(P12Next)
         # --------------------------------------------------------------------------------------------------------------
-
+        
         # Setting the window
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setWindowTitle(self.title)
         # --------------------------------------------------------------------------------------------------------------
-
+    
     # Function for creating the software loop window
     def SLMenu(self):
         # Global layout variables
@@ -141,7 +170,7 @@ class UI(QWidget):
         Software4 = QPushButton('Software 4')
         Software5 = QPushButton('Software 5')
         Software6 = QPushButton('Software 6')
-
+        
         # Setting the layout
         Grid = QGridLayout()
         Horizontal.addWidget(TEComputerNames)
@@ -152,9 +181,14 @@ class UI(QWidget):
         Grid.addWidget(Software5, 1, 1)
         Grid.addWidget(Software6, 1, 2)
         Horizontal.addLayout(Grid)
-
+        
         Vertical.addLayout(Horizontal)
-
+        
+    def P12Menu(self):
+        global Vertical
+        global Horizontal
+        
+    
 
 
 # Function that starts the application
